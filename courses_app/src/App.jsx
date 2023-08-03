e452d5df191e587888cd4829175da3ba1e40199e
luvSainii <beasaini2002@gmail.com>
2023-08-03 23:57:20 +0530

    new commit

--- courses_app/src/App.jsx
+++ courses_app/src/App.jsx
@@ -1,42 +1,42 @@
 import Cards from "./components/Cards";
 import Navbar from "./components/Navbar";
-import Filter from "./components/Filter"
+import Filter from "./components/Filter";
+import Spinner from "./components/Spinner";
 import "./App.css";
 import { apiUrl, filterData } from "./data"
 import { useEffect, useState } from "react";
 import { toast } from "react-toastify";
 
 function App() {
-  let [courses, setCourses] = useState(null)
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const res = await fetch(apiUrl)
-        const output = await res.json();
-          //save data into a variable
-          console.log(output);
-          setCourses(output.data)
-      } catch (error) {
-        toast.error("Something went wrong");
-      }
+  let [courses, setCourses] = useState(null);
+  let [loading, setLoading] = useState(true);
+  const fetchData = async () => {
+    try {
+      setLoading(true);
+      const res = await fetch(apiUrl)
+      const output = await res.json();
+      //save data into a variable
+      console.log(output);
+      setCourses(output.data)
+    } catch (error) {
+      toast.error("Something went wrong");
     }
+    setLoading(false)
+  }
+  useEffect(() => {
     fetchData();
   }, [])
-    if(courses==null){
-      <div className="loader">
-      <div className="glitch">Loading...</div>
-    </div>
-    }
-    else{
-      return (
-        <div className="Container">
-          <Navbar />
-          <Filter filterData={filterData} />
-          <Cards courses={courses} />
-        </div>
-      );
-    }
 
-}
+    return (
+      <div className="Container">
+        <div><Navbar /></div>
+        <div><Filter filterData={filterData} /></div>
+        <div>
+          { loading ?(<Spinner/>):(<Cards courses={courses} />)}
+        </div>
+      </div>
+    );
+  }
+
 
 export default App;
---
+++ random-gif/src/App.jsx
@@ -0,0 +1,9 @@
+
+
+export default function App() {
+  return (
+    <div className="w-full h-screen flex flex-col background items-center">
+      Hello Ji
+    </div>
+  );
+}
