4acbbef3ab24e687593af28b17c15e8675223344
luvSainii <beasaini2002@gmail.com>
2023-08-04 22:59:31 +0530

    adding some new things

---
+++ random-gif/src/components/Random.js
@@ -0,0 +1,35 @@
+import axios from 'axios';
+import React, { useEffect, useState } from 'react'
+import Spinner from './Spinner';
+const API_KEY = process.env.REACT_APP__GIPHY_API_KEY
+
+const Random = () => {
+    const [gif, setGif] = useState(null);
+    const [loading, setLoading] = useState(true);
+    async function fetchData() {
+        setLoading(true);
+        const url = `https://api.giphy.com/v1/gifs/random?api_key=${API_KEY}`;
+        const { data } = await axios.get(url);
+        const imageSource = data.data.images.downsized_large.url;
+        setGif(imageSource);
+        setLoading(false);
+    }
+    useEffect(() => {
+        fetchData();
+    }, [])
+
+    function gifHandler() {
+        fetchData();
+    }
+    return (
+        <div className="w-1/2 h-[450px] bg-green-500 rounded-lg border border-black flex flex-col items-center gap-y-5 mt-[15px]">
+            <h2 className='text-3xl underline uppercase font-bold'>A RANDOM GIF</h2>
+
+            <div className='w-[40px] h-[30px]'>{loading === true ? <Spinner/> :  <img src={gif} alt="" />}
+</div>
+            <button onClick={gifHandler} className='w-1/4 bg-white text-lg py-2 rounded-lg mb-[20px]'>Generate</button>
+        </div>
+    )
+}
+
+export default Random
